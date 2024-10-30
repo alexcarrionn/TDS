88e9db2ee1b8c3d4e0b73cb08a263d796c0ec5b7
alexcarrionn <acarrionjordan@gmail.com>
2024-10-30 12:23:41 +0100

    pasa de login a registro y cambio en regitro

--- appChat/src/main/java/appChat/Ventanas/VentanaRegistro.java
+++ appChat/src/main/java/appChat/Ventanas/VentanaRegistro.java
@@ -2,10 +2,9 @@ package appChat.Ventanas;
 
 import javax.swing.*;
 import java.awt.*;
+import com.toedter.calendar.JDateChooser;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.io.File;
-import com.toedter.calendar.JDateChooser;
 
 public class VentanaRegistro {
     private static GridBagConstraints gbc_1;
@@ -16,13 +15,17 @@ public class VentanaRegistro {
     private static GridBagConstraints gbc_7;
     private static GridBagConstraints gbc_8;
     private static GridBagConstraints gbc_AreaEstado;
-    private static GridBagConstraints gbc_9;
-    private static GridBagConstraints gbc_10;
+    private static JTextField textField;
+    private static GridBagConstraints gbc_4;
+    private static JTextField textField_1;
     private static GridBagConstraints gbc_11;
+    private static JTextField textField_2;
+    private static JTextField textField_3;
+    private JFrame frame;
 
-    public static void main(String[] args) {
+    public VentanaRegistro() {
         // Crear la ventana principal
-        JFrame frame = new JFrame("Ventana registro");
+    	frame = new JFrame("Ventana registro");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(582, 394);
         frame.getContentPane().setLayout(new BorderLayout());
@@ -31,10 +34,10 @@ public class VentanaRegistro {
         JPanel panelCentral = new JPanel();
         panelCentral.setForeground(Color.GREEN);
         GridBagLayout gbl_panelCentral = new GridBagLayout();
-        gbl_panelCentral.rowHeights = new int[]{0, 0, 0, 0, 31, 0, 0};
+        gbl_panelCentral.rowHeights = new int[]{0, 0, 0, 0, 31, 0, 30};
         gbl_panelCentral.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-        gbl_panelCentral.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0};
-        gbl_panelCentral.columnWidths = new int[]{0, 135, 0, 139};
+        gbl_panelCentral.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+        gbl_panelCentral.columnWidths = new int[]{0, 234, 0, 0, 139, 0};
         panelCentral.setLayout(gbl_panelCentral);
 
         // Etiqueta y campo de nombre
@@ -47,10 +50,9 @@ public class VentanaRegistro {
         
         gbc_2 = new GridBagConstraints();
         gbc_2.anchor = GridBagConstraints.WEST;
-        gbc_2.insets = new Insets(5, 5, 5, 0);
+        gbc_2.insets = new Insets(5, 5, 5, 5);
         gbc_2.gridx = 1;
         gbc_2.gridy = 0;
-        gbc_2.gridwidth = 3;
         JTextField nombreField = new JTextField(20);
         panelCentral.add(nombreField, gbc_2);
 
@@ -64,10 +66,9 @@ public class VentanaRegistro {
         
         gbc_3 = new GridBagConstraints();
         gbc_3.anchor = GridBagConstraints.WEST;
-        gbc_3.insets = new Insets(5, 5, 5, 0);
+        gbc_3.insets = new Insets(5, 5, 5, 5);
         gbc_3.gridx = 1;
         gbc_3.gridy = 1;
-        gbc_3.gridwidth = 3;
         JTextField apellidosField = new JTextField(20);
         panelCentral.add(apellidosField, gbc_3);
 
@@ -78,14 +79,14 @@ public class VentanaRegistro {
         gbc_7.gridx = 0;
         gbc_7.gridy = 2;
         panelCentral.add(new JLabel("Teléfono:"), gbc_7);
-        
-        gbc_10 = new GridBagConstraints();
-        gbc_10.anchor = GridBagConstraints.WEST;
-        gbc_10.insets = new Insets(5, 5, 5, 5);
-        gbc_10.gridx = 1;
-        gbc_10.gridy = 2;
-        JTextField telefonoField = new JTextField(10);
-        panelCentral.add(telefonoField, gbc_10);
+
+        textField_3 = new JTextField(20);
+        GridBagConstraints gbc_textField_3 = new GridBagConstraints();
+        gbc_textField_3.insets = new Insets(5, 5, 5, 5);
+        gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
+        gbc_textField_3.gridx = 1;
+        gbc_textField_3.gridy = 2;
+        panelCentral.add(textField_3, gbc_textField_3);
 
         // Etiqueta y campos de contraseña
         gbc_6 = new GridBagConstraints();
@@ -94,28 +95,32 @@ public class VentanaRegistro {
         gbc_6.gridx = 0;
         gbc_6.gridy = 3;
         panelCentral.add(new JLabel("Contraseña:"), gbc_6);
-        
-        gbc_9 = new GridBagConstraints();
-        gbc_9.anchor = GridBagConstraints.WEST;
-        gbc_9.insets = new Insets(5, 5, 5, 5);
-        gbc_9.gridx = 1;
-        gbc_9.gridy = 3;
-        JPasswordField passwordField1 = new JPasswordField(10);
-        panelCentral.add(passwordField1, gbc_9);
-        
-        gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5);
-        gbc.gridx = 2;
-        gbc.gridy = 3;
-        panelCentral.add(new JLabel("Repetir:"), gbc);
-        
+
+        textField_2 = new JTextField(20);
+        GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+        gbc_textField_2.insets = new Insets(5, 5, 5, 5);
+        gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+        gbc_textField_2.gridx = 1;
+        gbc_textField_2.gridy = 3;
+        panelCentral.add(textField_2, gbc_textField_2);
+
         gbc_11 = new GridBagConstraints();
-        gbc_11.anchor = GridBagConstraints.WEST;
-        gbc_11.insets = new Insets(5, 5, 5, 0);
+        gbc_11.anchor = GridBagConstraints.EAST;
+        gbc_11.insets = new Insets(5, 5, 5, 5);
         gbc_11.gridx = 3;
         gbc_11.gridy = 3;
-        JPasswordField passwordField2 = new JPasswordField(10);
-        panelCentral.add(passwordField2, gbc_11);
+        JLabel label_1 = new JLabel("Repetir:");
+        panelCentral.add(label_1, gbc_11);
+
+        textField_1 = new JTextField();
+        textField_1.setColumns(10);
+        GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+        gbc_textField_1.gridwidth = 2;
+        gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+        gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+        gbc_textField_1.gridx = 4;
+        gbc_textField_1.gridy = 3;
+        panelCentral.add(textField_1, gbc_textField_1);
 
         // Etiqueta y campo de fecha
         gbc_1 = new GridBagConstraints();
@@ -128,51 +133,37 @@ public class VentanaRegistro {
         JDateChooser dateChooser = new JDateChooser();
         dateChooser.setDateFormatString("dd/MM/yyyy");
         GridBagConstraints gbc_dateChooser = new GridBagConstraints();
-        gbc_dateChooser.fill = GridBagConstraints.HORIZONTAL; // Cambia BOTH a HORIZONTAL
-        gbc_dateChooser.insets = new Insets(5, 5, 5, 5); // Asegúrate de tener el mismo espaciado en todos los lados
+        gbc_dateChooser.fill = GridBagConstraints.HORIZONTAL;
+        gbc_dateChooser.insets = new Insets(5, 5, 5, 5);
         gbc_dateChooser.gridx = 1;
         gbc_dateChooser.gridy = 4;
         panelCentral.add(dateChooser, gbc_dateChooser);
-
+        
         // Etiqueta y espacio para la imagen
-        gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 0);
-        gbc.gridx = 3;
-        gbc.gridy = 4;
-        panelCentral.add(new JLabel("Imagen:"), gbc);
+        gbc_4 = new GridBagConstraints();
+        gbc_4.anchor = GridBagConstraints.EAST;
+        gbc_4.insets = new Insets(5, 5, 5, 5);
+        gbc_4.gridx = 3;
+        gbc_4.gridy = 4;
+        JLabel label = new JLabel("Imagen:");
+        panelCentral.add(label, gbc_4);
+
+        textField = new JTextField();
+        GridBagConstraints gbc_textField = new GridBagConstraints();
+        gbc_textField.gridwidth = 2;
+        gbc_textField.insets = new Insets(0, 0, 5, 0);
+        gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+        gbc_textField.gridx = 4;
+        gbc_textField.gridy = 4;
+        panelCentral.add(textField, gbc_textField);
+        textField.setColumns(10);
         
         gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 0);
-        gbc.gridx = 3;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.gridx = 4;
         gbc.gridy = 5;
         JLabel imagenLabel = new JLabel(new ImageIcon(VentanaRegistro.class.getResource("/imagenes/agregar-usuario.png")));
         panelCentral.add(imagenLabel, gbc);
-
-        // Botón para cargar la imagen
-        gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 0, 0);
-        gbc.gridx = 3;
-        gbc.gridy = 6;
-        JButton cargarImagenButton = new JButton("Añadir Imagen");
-        panelCentral.add(cargarImagenButton, gbc);
-
-        // Acción del botón para cargar la imagen
-        cargarImagenButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Crea un JFileChooser para seleccionar la imagen
-                JFileChooser fileChooser = new JFileChooser();
-                int returnValue = fileChooser.showOpenDialog(frame);
-
-                // Si se selecciona un archivo
-                if (returnValue == JFileChooser.APPROVE_OPTION) {
-                    File selectedFile = fileChooser.getSelectedFile();
-                    // Establecer la nueva imagen en el JLabel
-                    ImageIcon imageIcon = new ImageIcon(selectedFile.getAbsolutePath());
-                    imagenLabel.setIcon(imageIcon);
-                }
-            }
-        });
 
         // Etiqueta y campo de saludo
         gbc_5 = new GridBagConstraints();
@@ -183,13 +174,11 @@ public class VentanaRegistro {
         panelCentral.add(new JLabel("Estado:"), gbc_5);
         
         gbc_AreaEstado = new GridBagConstraints();
-        gbc_AreaEstado.anchor = GridBagConstraints.WEST;
+        gbc_AreaEstado.fill = GridBagConstraints.HORIZONTAL;
         gbc_AreaEstado.insets = new Insets(5, 5, 5, 5);
         gbc_AreaEstado.gridx = 1;
         gbc_AreaEstado.gridy = 5;
-        gbc_AreaEstado.gridwidth = 2;
         
-        // Añadir JTextArea dentro de JScrollPane
         JTextArea AreaEstado = new JTextArea(5, 20);
         AreaEstado.setWrapStyleWord(true);
         AreaEstado.setLineWrap(true);
@@ -197,21 +186,36 @@ public class VentanaRegistro {
         scrollPane.setPreferredSize(new Dimension(200, 100));
         panelCentral.add(scrollPane, gbc_AreaEstado);
         
-        // Añadir panel central al frame
+        // Agregar panel al marco
         frame.getContentPane().add(panelCentral, BorderLayout.CENTER);
-
-        // Crear el panel inferior para los botones
-        JPanel panelBotones = new JPanel();
-        JButton cancelButton = new JButton("Cancelar");
-        JButton acceptButton = new JButton("Aceptar");
-
-        panelBotones.add(cancelButton);
-        panelBotones.add(acceptButton);
-
-        // Añadir panel de botones al frame
-        frame.getContentPane().add(panelBotones, BorderLayout.SOUTH);
         
-        // Hacer la ventana visible
+        JPanel panel = new JPanel();
+        GridBagConstraints gbc_panel = new GridBagConstraints();
+        gbc_panel.insets = new Insets(0, 0, 0, 5);
+        gbc_panel.fill = GridBagConstraints.BOTH;
+        gbc_panel.gridx = 1;
+        gbc_panel.gridy = 6;
+        panelCentral.add(panel, gbc_panel);
+        
+        JButton btnNewButton = new JButton("Aceptar");
+        btnNewButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(btnNewButton);
+        
+        Component horizontalGlue = Box.createHorizontalGlue();
+        panel.add(horizontalGlue);
+        
+        JButton btnNewButton_1 = new JButton("Cancelar");
+        btnNewButton_1.addActionListener(e->System.exit(0));
+        panel.add(btnNewButton_1);
         frame.setVisible(true);
     }
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		frame.setVisible(b);
+	}
 }
