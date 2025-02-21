89dbd9f05342649d93d07088f2f89bfcf6ca0e57
alexcarrionn <acarrionjordan@gmail.com>
2025-02-21 13:02:46 +0100

    empezado lanzador

---
+++ appChat/src/main/java/lanzador/Lanzador.java
@@ -0,0 +1,14 @@
+package lanzador;
+
+//import java.io.IOException;
+
+/*public class Lanzador {
+	ProcessBuilder terminalPersistencia = new ProcessBuilder("cmd.exe", "/c", "start", "cmd.exe", "/k",
+			"java -jar ServidorPersistenciaH2.jar");
+	terminalPersistencia.directory(new File("."));
+	try {
+		terminalPersistencia.start();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+}*/
